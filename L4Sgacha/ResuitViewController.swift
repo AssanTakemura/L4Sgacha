7a2d48f0736f17f6b4b9d0cddeb0b72f9176e44a
Asuka Takemura <assan.takemura@gamil.com>
2020-09-03 01:32:52 +0900

    完成

---
+++ L4Sgacha/ResuitViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ResuitViewController.swift
+//  L4Sgacha
+//
+//  Created by 竹村明日香 on 2020/09/03.
+//  Copyright © 2020 Takemura assan. All rights reserved.
+//
+
+import UIKit
+
+class ResuitViewController: UIViewController {
+    
+    var number: Int!
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var monsterImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        number = Int.random(in: 0...9)
+        
+        if number == 9{
+            monsterImageView.image = UIImage(named: "monster010")
+            backgroundImageView.image = UIImage(named: "bg_gold")
+        }else if number > 7 {
+            monsterImageView.image = UIImage(named: "monster006")
+            backgroundImageView.image = UIImage(named: "bg_red")
+        }else {
+            monsterImageView.image = UIImage(named: "monster003")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+    }
+    
+    @IBAction func back(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
